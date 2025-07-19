9c53f5fdc61781ce7aac4ac751549988a55aa750
Caleb <xxy3429924750@126.com>
2025-07-19 22:42:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ntfy Desktop
+
+A simple & easy-to-use desktop client for [ntfy](https://github.com/binwiederhier/ntfy)
+
+By modifying `config.json`, you can subscribe to topics from different servers
+
+```json
+{
+    "server1": ["topic1", "topic2"],
+    "server2": ["topic3"]
+}
+```
---
+++ main.py
@@ -0,0 +1,46 @@
+from plyer import notification
+import platform
+import requests
+import threading
+import json
+
+def load_config(file="config.json"):
+    with open(file, "r") as f:
+      config_file = f.read()
+    try:
+        config_json = json.loads(config_file)
+        return config_json
+    except:
+        pass
+
+def send_notification(title, message):
+    system = platform.system()
+    notification.notify(
+        title=title,
+        message=message,
+        app_icon="msg.ico",
+        timeout=5
+    )
+
+def listen_to_topic(url, topic):
+    resp = requests.get(f"{url}/{topic}/json", stream=True)
+    for line in resp.iter_lines():
+        if line:
+            msg_str = line.decode('utf-8')
+            msg_json = json.loads(msg_str)
+            title = msg_json.get("title", f"{url.split('://')[-1]}/{topic}")
+            msg = msg_json.get("message", "")
+
+            if msg:
+                send_notification(title=title, message=msg)
+            print(msg_str)
+
+if __name__ == "__main__":
+    config_json = load_config()
+    for server, topics in config_json.items():
+        for topic in topics:
+            thread = threading.Thread(target=listen_to_topic, args=(server, topic), daemon=True)
+            thread.start()
+    
+    while True:
+        pass
